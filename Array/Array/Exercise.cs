b88ecddf13ba4970b2d6c67af90d4ea217a5e578
Vadym <Vadymshmorgun23@gmail.com>
2017-06-22 16:14:48 +0300

    Add

---
+++ Array/Array/Exercise.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Exercise : IExercise
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Enter array size: ");
+            int size = int.Parse(Console.ReadLine());
+
+            Int32[] array = new Int32[size];
+
+            bool isFilled = false;
+
+            bool[] array2 = new bool[size];
+
+            while (!isFilled)
+            {
+                Console.WriteLine("Enter number");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter index of array: ");
+                int indexOfArray = int.Parse(Console.ReadLine());
+
+
+                array[indexOfArray] = number;
+                array2[indexOfArray] = true;
+                isFilled = true;
+
+                for (int i = 0; i < array2.Length; i++)
+                {
+                    if (array2[i] == false)
+                    {
+                        isFilled = false;
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Here is your array: " + array[i]);
+            }
+
+            Console.ReadKey();
+        }
+
+        public string Description()
+        {
+            return "In this exercise we created array";
+        }        
+    }
+}
--- Array/Array/Program.cs
+++ Array/Array/Program.cs
@@ -7,51 +7,12 @@ using System.Threading.Tasks;
 namespace Array
 {
     class Program
-    {
-        
-
+    {        
         static void Main(string[] args)
         {
-           
-            Console.WriteLine("Enter array size: ");
-            int size = int.Parse(Console.ReadLine());
-
-            Int32[] array = new Int32[size];
-
-            bool isFilled = false;
-
-            bool[] array2 = new bool[size];
-
-            while (!isFilled)
-            {
-                Console.WriteLine("Enter number");
-                int number = int.Parse(Console.ReadLine());
-
-                Console.WriteLine("Enter index of array: ");
-                int indexOfArray = int.Parse(Console.ReadLine());
-
-
-                array[indexOfArray] = number;
-                array2[indexOfArray] = true;
-                isFilled = true;
-
-                for(int i = 0; i < array2.Length; i++)
-                {
-                    if(array2[i] == false)
-                    {
-                        isFilled = false;
-                        break;
-                    }
-                }
-            }
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.WriteLine(array[i]);
-            }
-
-            Console.ReadKey();
-
+            Exercise ex1 = new Exercise();
+            ex1.Execute();
+            ex1.Description();
         }
     }
 }
